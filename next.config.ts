1236d724b6562e9e4c55859687ff2b603c6d1499
Darren Eaton <darrelleaton18@googlemail.com>
2026-08-06 23:16:56 +0100

    Fix basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // If you decide to use the <Image /> component later, you'll also need this:
-  // images: { unoptimized: true } 
+  output: 'export',
+  basePath: '/NexaKit',
+  assetPrefix: '/NexaKit/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
