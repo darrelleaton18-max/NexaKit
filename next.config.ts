a94b8fc56750b139023944d3131aac8078cfff49
Darren Eaton <darrelleaton18@googlemail.com>
2026-08-06 20:33:10 +0100

    Configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // If you decide to use the <Image /> component later, you'll also need this:
+  // images: { unoptimized: true } 
 };
 
 export default nextConfig;
